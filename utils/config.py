0860b0772b836772115313d5c7f5971a61b852e1
Nathan Van Maastricht <nathan.vanmaastricht@gmail.com>
2024-08-04 16:20:09 +0930

    Removed test code from config.py

--- utils/config.py
+++ utils/config.py
@@ -5,9 +5,3 @@ def load_config(config_file_path):
     with open(config_file_path, "r") as file:
         config = json.load(file)
     return config
-
-
-config_file_path = "config.json"
-config = load_config(config_file_path)
-
-print(config)
